43c6690de0f0105308261e54df8b81308e9a13ca
Ankit Kumar <ak8415129@gmail.com>
2025-12-01 12:35:10 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+ content: ["./**/*.{html,js}"],
+ theme: {
+ extend: {
+ colors: {
+ google: {
+ blue: 'hashtag#4285F4',
+ red: 'hashtag#EA4335',
+ yellow: 'hashtag#FBBC05',
+ green: 'hashtag#34A853',
+ gray: 'hashtag#5f6368',
+ lightgray: 'hashtag#f1f3f4'
+ }
+ },
+ fontFamily: {
+ sans: ['Roboto', 'sans-serif'],
+ }
+ },
+ },
+ plugins: [],
+}
+
+
